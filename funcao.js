f54f982ca0e1ae27ac2d97663d1dac3d8254c7ca
dopamineeeee <eric.oliveiraferreira@hotmail.com>
2026-05-13 22:12:20 -0300

    teste

--- funcao.js
+++ funcao.js
@@ -5,3 +5,36 @@ botoes.forEach((botao) => {
         alert("Você clicou no botão!");
     });
 });
+
+
+const elemento = {
+
+email: document.querySelector("#email"),
+senha: document.querySelector("#senha"),
+form: document.querySelector("#form"),
+button: document.querySelector("#button"),
+
+
+};
+
+const dadosDigitados = {
+
+email:"",
+senha:"",
+
+};
+
+
+elemento.form.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    // getDadosDigitados(elemento.email.value, elemento.senha.value);
+    console.log((elemento.email.value, elemento.senha.value))
+});
+
+function getDadosDigitados(email,senha)
+{
+
+    dadosDigitados.email = email;
+    dadosDigitados.senha = senha;
+
+}
