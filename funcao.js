6ccf538280ba8ba96999af22c9410acef22a5498
dopamineeeee <eric_oliveiraferreira@hotmail.com>
2026-05-15 03:16:29 -0300

    casa

--- funcao.js
+++ funcao.js
@@ -38,3 +38,4 @@ function getDadosDigitados(email,senha)
     dadosDigitados.senha = senha;
 
 }
+
---
+++ login2.js
@@ -0,0 +1,69 @@
+const btnConsulta = document.getElementById("btnConsulta");
+
+const inicio = document.getElementById("inicio");
+
+const consulta = document.getElementById("consulta");
+
+btnConsulta.addEventListener("click", () => {
+
+    inicio.style.display = "none";
+
+    consulta.style.display = "flex";
+
+});
+
+const btnvoltar = document.getElementById("btnvoltar");
+
+btnvoltar.addEventListener("click", () => {
+
+    consulta.style.display = "none";
+
+    inicio.style.display = "flex";
+
+});
+
+
+// ===============================
+// BUSCAR PRODUTO
+// ===============================
+
+const btnBuscar = document.getElementById("btnBuscar");
+
+btnBuscar.addEventListener("click", buscarProduto);
+
+async function buscarProduto() {
+
+    const valor = document
+        .getElementById("campoProduto")
+        .value
+        .toLowerCase();
+
+    const resposta = await fetch("produtos.json");
+
+    const produtos = await resposta.json();
+
+    const produtoEncontrado = produtos.find(produto =>
+        produto.nome.toLowerCase() === valor
+    );
+
+    const resultado = document.getElementById("resultado");
+
+    if(produtoEncontrado) {
+
+        resultado.innerHTML = `
+
+            <h2>${produtoEncontrado.nome}</h2>
+
+            <p>Preço: R$ ${produtoEncontrado.preco}</p>
+
+        `;
+
+    } else {
+
+        resultado.innerHTML = `
+
+            <p>Produto não encontrado</p>
+
+        `;
+    }
+}
